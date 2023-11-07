62ba0ba778a18abe27009d19be138197d50ba5cb
kansal-tushar <tusharkansal14@gmail.com>
2023-11-07 14:08:59 +0530

    Project setup

---
+++ src/test/java/com/ecommerce/major/MajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.major;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
