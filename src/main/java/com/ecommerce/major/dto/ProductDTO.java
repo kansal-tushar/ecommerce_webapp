7f5f3ce812be20da503ae62dc02ecc98c9c93c73
kansal-tushar <tusharkansal14@gmail.com>
2023-11-08 11:31:41 +0530

    Project setup

---
+++ src/main/java/com/ecommerce/major/dto/ProductDTO.java
@@ -0,0 +1,27 @@
+package com.ecommerce.major.dto;
+
+import com.ecommerce.major.model.Category;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+public class ProductDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private int categoryId;
+
+    private double price;
+
+    private double weight;
+
+    private String description;
+
+    private String imageName;
+
+}
---
+++ src/main/java/com/ecommerce/major/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.major.repository;
+
+import com.ecommerce.major.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
+    List<Product> findAllByCategory_Id(int id);
+}
